8e78217a88a5441f0479f78577513ba19be5a9b6
rudraarora <rudra.arora.ipu@hmail.com>
2026-02-14 18:24:12 +0530

    added superuser

--- apps/user/apps.py
+++ apps/user/apps.py
@@ -6,3 +6,16 @@ from django.apps import AppConfig
 class UserConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.user"
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
+
+        if not User.objects.filter(name="admin").exists():
+            User.objects.create_superuser(
+                name="admin",
+                email="admin@test.com",
+                password="Admin@123",
+                phone_number=1234567890
+            )
